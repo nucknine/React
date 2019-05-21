9c3a1f476c80845d192d8e6033f0aafc2b929a5a
nucknine <nucknine@gmail.com>
2019-05-21 16:53:42 +0300

    complete

--- routing--assignment/src/containers/Course/Course.js
+++ routing--assignment/src/containers/Course/Course.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+  state = {
+    title: null
+  };
+
+  componentDidMount() {
+    this.updateQueryParams();
+  }
+
+  componentDidUpdate() {
+    this.updateQueryParams();
+  }
+
+  updateQueryParams() {
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      if (param[0] === "title") {
+        if (param[1] !== this.state.title) {
+          this.setState({ title: param[1] });
+        }
+      }
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>You selected the Course with ID: {this.props.match.params.id}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
--- routing--assignment/src/containers/Courses/Courses.js
+++ routing--assignment/src/containers/Courses/Courses.js
@@ -1,30 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { NavLink, Route } from "react-router-dom";
+import Course from "../Course/Course";
 
-import './Courses.css';
+import "./Courses.css";
 
 class Courses extends Component {
-    state = {
-        courses: [
-            { id: 1, title: 'Angular - The Complete Guide' },
-            { id: 2, title: 'Vue - The Complete Guide' },
-            { id: 3, title: 'PWA - The Complete Guide' }
-        ]
-    }
+  state = {
+    courses: [
+      { id: 1, title: "Angular - The Complete Guide" },
+      { id: 2, title: "Vue - The Complete Guide" },
+      { id: 3, title: "PWA - The Complete Guide" }
+    ]
+  };
 
-    render () {
-        return (
-            <div>
-                <h1>Amazing Udemy Courses</h1>
-                <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
-                </section>
-            </div>
-        );
-    }
+  render() {
+    const links = this.state.courses.map(course => {
+      return (
+        <NavLink
+          key={course.id}
+          to={{
+            pathname: "/courses/course/" + course.id,
+            search: "?id=3&title=" + course.title
+          }}
+        >
+          <article className="Course">{course.title}</article>
+        </NavLink>
+      );
+    });
+    return (
+      <React.Fragment>
+        <h1>Amazing Udemy Courses</h1>
+        <section className="Courses">{links}</section>
+        <hr />
+        <Route
+          path={this.props.match.url + "/course/:id"}
+          exact
+          component={Course}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Courses;
