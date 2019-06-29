ee379e5021d01a53d673bace75c6b57af4eb4151
Oleg_Maximov <nucknine@gmail.com>
2019-06-29 15:31:06 +0300

    added testing examples

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions/index";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   // old syntax
   // constructor(props) {
   //   super(props);
